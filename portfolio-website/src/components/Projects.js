e448434607ce7a5c7e894a78cc01c8faadfeb030
Vihanga Malinda Fernando <vihangamalindafernando@gmail.com>
2022-08-09 04:59:45 +0530

    CSS animation with Animate.css

--- portfolio-website/src/components/Projects.js
+++ portfolio-website/src/components/Projects.js
@@ -4,6 +4,8 @@ import colorSharp2 from "../assets/img/color-sharp2.png";
 import projImg1 from "../assets/img/project-img1.png";
 import projImg2 from "../assets/img/project-img2.png";
 import projImg3 from "../assets/img/project-img3.png";
+import "animate.css";
+import TrackVisibility from "react-on-screen";
 
 
 export const Projects = () => {
@@ -30,47 +32,60 @@ export const Projects = () => {
       <Container>
         <Row>
           <Col>
-            <h2>Projects</h2>
-            <p>
-              Generates passages of lorem ipsum text suitable for use as
-              placeholder copy in web pages, graphics, and more. Works in the
-              browser, NodeJS, and React Native.
-            </p>
+            <TrackVisibility>
+              {({ isVisible }) => (
+                <div
+                  className={
+                    isVisible ? "animate__animated animate__bounce" : ""
+                  }
+                >
+                  <h2>Projects</h2>
+                  <p>
+                    Generates passages of lorem ipsum text suitable for use as
+                    placeholder copy in web pages, graphics, and more. Works in
+                    the browser, NodeJS, and React Native.
+                  </p>
+                </div>
+              )}
+            </TrackVisibility>
             <Tab.Container id="projects-tabs" defaultActiveKey="first">
-            <Nav variant="pills"  className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
-              <Nav.Item>
-                <Nav.Link eventKey="first">Tab One</Nav.Link>
-              </Nav.Item>
-              <Nav.Item>
-                <Nav.Link eventKey="second">Tab Two</Nav.Link>
-              </Nav.Item>
-              <Nav.Item>
-                <Nav.Link eventKey="third">Tab Three</Nav.Link>
-              </Nav.Item>
-            </Nav>
-            <Tab.Content>
+              <Nav
+                variant="pills"
+                className="nav-pills mb-5 justify-content-center align-items-center"
+                id="pills-tab"
+              >
+                <Nav.Item>
+                  <Nav.Link eventKey="first">Tab One</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey="second">Tab Two</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey="third">Tab Three</Nav.Link>
+                </Nav.Item>
+              </Nav>
+              <Tab.Content>
                 <Tab.Pane eventKey="first">
-                    <Row>
-                        {
-                            projects.map((project,index)=>{
-                                return(
-                                    <ProjectCard
-                                    key={index}
-                                    {...project}
-                                    ></ProjectCard>
-                                )
-                            })
-                        }
-                    </Row>
+                  <Row>
+                    {projects.map((project, index) => {
+                      return (
+                        <ProjectCard key={index} {...project}></ProjectCard>
+                      );
+                    })}
+                  </Row>
                 </Tab.Pane>
                 <Tab.Pane eventKey="second">Loren nnnn</Tab.Pane>
                 <Tab.Pane eventKey="third">Loren nnnn</Tab.Pane>
-            </Tab.Content>
+              </Tab.Content>
             </Tab.Container>
           </Col>
         </Row>
       </Container>
-      <img className="background-image-right" src={colorSharp2} alt="Background"/>
+      <img
+        className="background-image-right"
+        src={colorSharp2}
+        alt="Background"
+      />
     </section>
   );
 };
